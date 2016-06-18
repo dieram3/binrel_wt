f8dc9b66dbf7422552f028106ebd1b512ff838c0
Diego <dieram3@gmail.com>
2016-06-17 21:47:03 -0400

    Added project base:
    - A .clang-format file for formatting purposes in c++
    - A .clang-tidy file for linter purposes in c++
    - A small .gitignore file
    - A cmake module with convenience functions (CPL_functions.cmake)
    - And an initial version of the main CMake file

---
+++ cmake/CPL_functions.cmake
@@ -0,0 +1,37 @@
+function(cpl_enable_all_warnings TARGET_ID)
+  if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    target_compile_options(${TARGET_ID} PRIVATE
+      -Weverything
+      -Wno-c++98-compat
+      -Wno-c++98-compat-pedantic
+      -Wno-padded
+    )
+  elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    target_compile_options(${TARGET_ID} PRIVATE
+      -Wall
+      -Wextra
+    )
+  endif()
+endfunction()
+
+function(cpl_make_cpp14_strict TARGET_ID)
+  set_property(TARGET ${TARGET_ID} PROPERTY CXX_EXTENSIONS       OFF)
+  set_property(TARGET ${TARGET_ID} PROPERTY CXX_STANDARD          14)
+  set_property(TARGET ${TARGET_ID} PROPERTY CXX_STANDARD_REQUIRED ON)
+endfunction()
+
+# Default configuration for CPL targets.
+function(cpl_configure TARGET_ID)
+  cpl_enable_all_warnings(${TARGET_ID})
+  cpl_make_cpp14_strict(${TARGET_ID})
+endfunction()
+
+function(cpl_add_library TARGET_ID)
+  add_library(${TARGET_ID} ${ARGN})
+  cpl_configure(${TARGET_ID})
+endfunction()
+
+function(cpl_add_executable TARGET_ID)
+  add_executable(${TARGET_ID} ${ARGN})
+  cpl_configure(${TARGET_ID})
+endfunction()
