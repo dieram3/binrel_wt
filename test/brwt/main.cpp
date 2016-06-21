c798128c3655b40cefbfad65eadc93794ee98795
Diego <dieram3@gmail.com>
2016-06-21 19:36:59 -0400

    Removed per file licenses

--- test/brwt/main.cpp
+++ test/brwt/main.cpp
@@ -1,7 +1,2 @@
-//          Copyright DJP Team 2015
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
