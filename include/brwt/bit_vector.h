83f27553c5f95b0c0e4167594d628ba39ff02f51
Diego <dieram3@gmail.com>
2016-07-02 20:29:22 -0400

    Added block access to bit_vector

--- include/brwt/bit_vector.h
+++ include/brwt/bit_vector.h
@@ -3,6 +3,7 @@
 
 #include <cstddef> // ptrdiff_t
 #include <cstdint> // uint_fast64_t
+#include <limits>  // numeric_limits
 #include <string>  // string
 #include <vector>  // vector
 
@@ -12,6 +13,10 @@ class bit_vector {
 public:
   using size_type = std::ptrdiff_t;
   using block_type = std::uint_fast64_t;
+
+public:
+  static constexpr size_type bits_per_block =
+      std::numeric_limits<block_type>::digits;
 
 public:
   bit_vector() = default;
@@ -28,6 +33,9 @@ public:
   block_type get_chunk(size_type pos, size_type count) const noexcept;
   void set_chunk(size_type pos, size_type count, block_type value) noexcept;
 
+  block_type get_block(size_type num_block) const noexcept;
+  void set_block(size_type num_block, const block_type value) noexcept;
+
 private:
   size_type m_len{};
   std::vector<block_type> blocks;
@@ -37,8 +45,18 @@ private:
 // Inline definitions
 // ==========================================
 
-inline bit_vector::size_type bit_vector::length() const noexcept {
+inline auto bit_vector::length() const noexcept -> size_type {
   return m_len;
+}
+
+inline auto bit_vector::get_block(const size_type num_block) const noexcept
+    -> block_type {
+  return blocks[static_cast<std::size_t>(num_block)];
+}
+
+inline void bit_vector::set_block(const size_type num_block,
+                                  const block_type value) noexcept {
+  blocks[static_cast<std::size_t>(num_block)] = value;
 }
 
 } // end namespace brwt
