65968e04a7b5313824e2600537e351c7d57cee4a
Diego <dieram3@gmail.com>
2016-07-06 15:27:32 -0400

    Added a bit of documentation to node_desc

--- include/brwt/wavelet_tree.h
+++ include/brwt/wavelet_tree.h
@@ -9,9 +9,15 @@ namespace brwt {
 
 class wavelet_tree {
 public:
+  /// Unsigned type used to represent symbol codes.
   using symbol_id = bit_vector::block_type;
+
+  /// Signed vocabulary type used to indicate quantities.
   using size_type = std::ptrdiff_t;
-  using index_type = std::ptrdiff_t;
+
+  /// Signed vocabulary type used to indicate positions.
+  using index_type = size_type;
+
   class node_desc;
 
 public:
@@ -78,29 +84,78 @@ public:
   node_desc make_root() const noexcept;
 
 private:
-  bitmap table{};      // Representation of the wavelet tree without pointers.
-  size_type seq_len{}; // The length of the original sequence.
-  size_type bits_per_symbol{}; // The number of bits per symbol used in *this.
+  /// Representation of the wavelet tree without pointers.
+  bitmap table{};
+
+  /// The length of the original sequence.
+  size_type seq_len{};
+
+  /// The number of bits per symbol used in <tt>*this</tt>.
+  size_type bits_per_symbol{};
 };
 
+/// \brief Proxy class to access the nodes of a wavelet tree.
+///
+/// Note that the node descriptors allow exploring the wavelet tree regardless
+/// of the internal structure.
+///
 class wavelet_tree::node_desc {
 public:
+  /// \brief Constructs the root node of the given wavelet tree.
+  ///
   explicit node_desc(const wavelet_tree& wt) noexcept;
 
   // internal bitmap access
+
+  /// \brief Retrieves the specified bit from this node bitmap.
+  ///
   bool access(size_type pos) const noexcept;
+
+  /// \brief Invokes \c rank_0 on this node bitmap.
+  ///
   size_type rank_0(index_type pos) const noexcept;
+
+  /// \brief Invokes \c rank_1 on this node bitmap.
+  ///
   size_type rank_1(index_type pos) const noexcept;
+
+  /// \brief Invokes \c select_0 on this node bitmap.
+  ///
   index_type select_0(size_type nth) const noexcept;
+
+  /// \brief Invokes \c select_1 on this node bitmap.
+  ///
   index_type select_1(size_type nth) const noexcept;
+
+  /// \brief Retrieves the size of this node bitmap.
+  ///
   size_type size() const noexcept;
 
   // Level information
+
+  /// \brief Checks whether the node has no materialized children.
+  ///
   bool is_leaf() const noexcept;
+
+  /// \brief Checks if the next bit of the symbol is handled by the left child.
+  ///
+  /// Note that this function can be called even on a leaf node because it does
+  /// not need its children to be materialized.
+  ///
   bool is_lhs_symbol(symbol_id symbol) const noexcept;
 
   // Navigation
+
+  /// \brief Constructs a proxy to the left hand side child.
+  ///
+  /// \pre <tt>!is_leaf()</tt>
+  ///
   node_desc make_lhs() const noexcept;
+
+  /// \brief Constructs a proxy to the right hand side child.
+  ///
+  /// \pre <tt>!is_leaf()</tt>
+  ///
   node_desc make_rhs() const noexcept;
 
 private:
