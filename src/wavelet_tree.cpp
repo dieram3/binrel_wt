94d83bdfc4d51affef309cb42d94e99e171ad76e
Diego Ramírez <dieram3@gmail.com>
2016-07-08 18:18:47 -0400

    Feature/wavelet tree (#8)
    * Added wavelet_tree API (at least first version)
    
    * Added constructor with int_vector and a test (not very useful though)
    
    * Corrected mispelled word
    
    * Added access
    
    * Implemented rank member function
    
    * Changed symbol_type to symbol_id, and changed sign type
    
    * Clarified sign conversion of wt test
    
    * Removed some old static casts
    
    * Implemented is_lhs_symbol private member function
    
    * Added select test
    
    * Implemented Select
    
    * Improved wavelet tree abstraction
    
    * Corrected some comments
    
    * Changed magic number from static_vector capacity to bits_per_symbol
    
    * Moved static_vector to independent file
    
    * Minor changed to static_vector
    
    * Symbols are now in terms of bits. The wavelet tree is navigable
    
    * Removed done TODO
    
    * Removed defaulted constructors from node_desc
    
    * Added wavelet tree navigation test
    
    * Renamed length to size. Added some convenience functions
    
    * Minor corrections
    
    * Added more assertions to implementation
    
    * Added tests for size, bpe and max_symbol
    
    * Added some static assert over default member function to wt
    
    * Added static asserts fro node_desc
    
    * Defined node_desc outside the class
    
    * Added a bit of documentation to node_desc
    
    * Renamed node_desc to node_proxy
    
    * Added a bit more of docs to the wavelet tree
    
    * Misc minor changes

---
+++ src/wavelet_tree.cpp
@@ -0,0 +1,304 @@
+#include <brwt/wavelet_tree.h>
+
+#include "static_vector.h" // static_vector
+#include <cassert>         // assert
+#include <cstddef>         // size_t
+#include <limits>          // numeric_limits
+#include <utility>         // move, exchange
+#include <vector>          // vector
+
+using brwt::wavelet_tree;
+using node_proxy = wavelet_tree::node_proxy;
+
+// ==========================================
+// Auxiliary algorithms
+// ==========================================
+
+template <typename InputIt, typename OutputIt, typename T>
+static void exclusive_scan(InputIt first, const InputIt last, OutputIt d_first,
+                           T init) {
+  for (; first != last; ++first) {
+    *d_first++ = std::exchange(init, init + (*first));
+  }
+}
+
+// ==========================================
+// wavelet_vector implementation
+// ==========================================
+
+wavelet_tree::wavelet_tree(const int_vector& sequence)
+    : table{}, seq_len{sequence.length()}, bits_per_symbol{sequence.get_bpe()} {
+  assert(bits_per_symbol >= 1);
+  using std::size_t;
+
+  // TODO(diegoramirez): Improves the constructor implementation. Consider
+  // represent the tree with a Wavelet matrix.
+
+  const auto alphabet_size = max_symbol_id() + 1;
+  std::vector<size_type> next_pos(2 * alphabet_size);
+
+  for (size_type i = 0; i < sequence.length(); ++i) {
+    const auto symbol = sequence[i];
+    ++next_pos[alphabet_size + symbol];
+  }
+  {
+    const auto first = next_pos.begin() + static_cast<size_type>(alphabet_size);
+    exclusive_scan(first, next_pos.end(), first, size_type{0});
+  }
+  for (auto j = alphabet_size - 1; j > 0; --j) {
+    const auto lhs = 2 * j; // rhs would be (2 * j + 1)
+    next_pos[j] = next_pos[lhs];
+  }
+
+  // Finally we can fill the table.
+  bit_vector bit_seq(bits_per_symbol * seq_len);
+  auto push_symbol = [&](const symbol_id symbol) {
+    symbol_id j = 1;
+    symbol_id base_symbol = 0;
+    auto num_symbols = alphabet_size;
+    size_type level_pos = 0;
+
+    while (num_symbols > 1) {
+      const auto lhs_symbols = num_symbols / 2;
+      const bool is_lhs = (symbol < base_symbol + lhs_symbols);
+
+      bit_seq.set(level_pos + (next_pos[j]++), !is_lhs);
+
+      if (is_lhs) {
+        j = 2 * j;
+        // base_symbol = base_symbol; // does not change
+        num_symbols = lhs_symbols;
+      } else {
+        j = 2 * j + 1;
+        base_symbol = (base_symbol + lhs_symbols);
+        num_symbols = (num_symbols - lhs_symbols);
+      }
+      level_pos += seq_len;
+    }
+    assert(j == alphabet_size + symbol);
+    assert(base_symbol == symbol);
+    assert(num_symbols == 1);
+    assert(level_pos == bit_seq.length());
+  };
+  for (size_type i = 0; i < sequence.length(); ++i) {
+    push_symbol(sequence[i]);
+  }
+
+  table = bitmap(std::move(bit_seq)); // The final magic.
+}
+
+auto wavelet_tree::access(index_type pos) const noexcept -> symbol_id {
+  assert(pos >= 0 && pos < size());
+
+  node_proxy node = make_root();
+  symbol_id res = 0;
+  while (!node.is_leaf()) {
+    // each iteration invokes rank three times.
+    if (!node.access(pos)) {
+      // res |= 0;
+      pos = node.rank_0(pos) - 1; // 1 rank
+      node = node.make_lhs();     // 2 ranks
+    } else {
+      res |= 1;
+      pos = node.rank_1(pos) - 1; // 1 rank
+      node = node.make_rhs();     // 2 ranks
+    }
+    res <<= 1;
+  }
+  res |= (node.access(pos)) ? 1 : 0;
+  return res;
+}
+
+auto wavelet_tree::rank(const symbol_id symbol, index_type pos) const noexcept
+    -> size_type {
+  assert(symbol <= max_symbol_id());
+  assert(pos >= 0 && pos < size());
+
+  node_proxy node = make_root();
+  while (!node.is_leaf()) {
+    // each iteration invokes rank three times.
+    if (node.is_lhs_symbol(symbol)) {
+      pos = node.rank_0(pos) - 1; // 1 rank
+      if (pos == -1) {
+        return 0;
+      }
+      node = node.make_lhs(); // 2 ranks
+    } else {
+      pos = node.rank_1(pos) - 1; // 1 rank
+      if (pos == -1) {
+        return 0;
+      }
+      node = node.make_rhs(); // 2 ranks
+    }
+  }
+  return node.is_lhs_symbol(symbol) ? node.rank_0(pos) : node.rank_1(pos);
+}
+
+auto wavelet_tree::select(const symbol_id symbol, const size_type nth) const
+    noexcept -> index_type {
+  assert(symbol <= max_symbol_id());
+  assert(nth > 0);
+  // Time complexity: Exactly 2 bitmap ranks and 1 bitmap select for level.
+
+  constexpr size_t max_bits_per_symbol = std::numeric_limits<symbol_id>::digits;
+  static_vector<node_proxy, max_bits_per_symbol> stack;
+
+  stack.emplace_back(make_root());
+  while (true) {
+    const auto& node = stack.back();
+    if (node.size() < nth) {
+      return -1;
+    }
+    if (node.is_leaf()) {
+      break;
+    }
+    stack.emplace_back(node.is_lhs_symbol(symbol) ? node.make_lhs()
+                                                  : node.make_rhs());
+  }
+
+  auto pos = [&] {
+    const auto& node = stack.back();
+    assert(node.size() >= nth);
+    return node.is_lhs_symbol(symbol) ? node.select_0(nth) : node.select_1(nth);
+  }();
+  if (pos == -1) {
+    return -1; // such element does not exists.
+  }
+
+  stack.pop_back();
+  while (!stack.empty()) {
+    const auto& node = stack.back();
+    pos = node.is_lhs_symbol(symbol) ? node.select_0(pos + 1)
+                                     : node.select_1(pos + 1);
+    assert(pos >= 0 && pos < node.size()); // The element is supposed to exist.
+    stack.pop_back();
+  }
+  return pos;
+};
+
+auto wavelet_tree::get_bits_per_symbol() const noexcept -> int {
+  return static_cast<int>(bits_per_symbol);
+}
+
+auto wavelet_tree::max_symbol_id() const noexcept -> symbol_id {
+  using limits = std::numeric_limits<symbol_id>;
+  return bits_per_symbol == limits::digits
+             ? limits::max()
+             : (symbol_id{1} << bits_per_symbol) - 1;
+}
+
+// ==========================================
+// node_proxy implementation
+// ==========================================
+
+node_proxy::node_proxy(const wavelet_tree& wt) noexcept
+    : wt_ptr{&wt},
+      range_begin{0},
+      range_size{wt.seq_len},
+      num_ones_before{0},
+      level_mask{symbol_id{1} << (wt.bits_per_symbol - 1)} {
+  assert(wt.bits_per_symbol >= 1);
+}
+
+auto node_proxy::access(const index_type pos) const noexcept -> bool {
+  assert(pos >= 0 && pos < size());
+  return get_table().access(begin() + pos);
+}
+
+// This function invokes table.rank_0 once.
+auto node_proxy::rank_0(const index_type pos) const noexcept -> size_type {
+  assert(pos >= 0 && pos < size());
+  return get_table().rank_0(begin() + pos) - zeros_before();
+}
+
+// This function invokes table.rank_1 once.
+auto node_proxy::rank_1(const index_type pos) const noexcept -> size_type {
+  assert(pos >= 0 && pos < size());
+  return get_table().rank_1(begin() + pos) - ones_before();
+}
+
+auto node_proxy::select_0(const size_type nth) const noexcept -> index_type {
+  assert(nth > 0);
+  const auto abs_pos = get_table().select_0(zeros_before() + nth);
+  if (abs_pos == -1 || abs_pos >= end()) {
+    return -1;
+  }
+  return abs_pos - begin();
+}
+
+auto node_proxy::select_1(const size_type nth) const noexcept -> index_type {
+  assert(nth > 0);
+  const auto abs_pos = get_table().select_1(ones_before() + nth);
+  if (abs_pos == -1 || abs_pos >= end()) {
+    return -1;
+  }
+  return abs_pos - begin();
+}
+
+auto node_proxy::size() const noexcept -> size_type {
+  return range_size;
+}
+
+auto node_proxy::is_leaf() const noexcept -> bool {
+  return level_mask == 1;
+}
+
+auto node_proxy::is_lhs_symbol(const symbol_id symbol) const noexcept -> bool {
+  return (symbol & level_mask) == 0;
+}
+
+// This function invokes table rank twice.
+auto node_proxy::make_lhs() const noexcept -> node_proxy {
+  assert(!is_leaf());
+  const auto first = begin() + wt_ptr->seq_len;
+  return node_proxy(/*wt_=*/*wt_ptr,
+                    /*begin_=*/first,
+                    /*size_=*/count_zeros(),
+                    /*ones_before_=*/get_table().rank_1(first - 1),
+                    /*level_mask_=*/(level_mask >> 1));
+}
+
+// This function invokes table rank twice.
+auto node_proxy::make_rhs() const noexcept -> node_proxy {
+  assert(!is_leaf());
+  const auto num_zeros = count_zeros();
+  const auto first = (begin() + wt_ptr->seq_len) + num_zeros;
+  return node_proxy(/*wt_=*/*wt_ptr,
+                    /*begin_=*/first,
+                    /*size_=*/(size() - num_zeros),
+                    /*ones_before_=*/get_table().rank_1(first - 1),
+                    /*level_mask_=*/(level_mask >> 1));
+}
+
+node_proxy::node_proxy(const wavelet_tree& wt_, const index_type begin_,
+                       const size_type size_, const size_type ones_before_,
+                       const symbol_id level_mask_) noexcept
+    : wt_ptr{&wt_},
+      range_begin{begin_},
+      range_size{size_},
+      num_ones_before{ones_before_},
+      level_mask{level_mask_} {}
+
+auto node_proxy::begin() const noexcept -> index_type {
+  return range_begin;
+}
+
+auto node_proxy::end() const noexcept -> index_type {
+  return begin() + size();
+}
+
+auto node_proxy::zeros_before() const noexcept -> size_type {
+  return begin() - ones_before();
+}
+
+auto node_proxy::ones_before() const noexcept -> size_type {
+  return num_ones_before;
+}
+
+auto node_proxy::count_zeros() const noexcept -> size_type {
+  return rank_0(size() - 1);
+}
+
+auto node_proxy::get_table() const noexcept -> const bitmap& {
+  return wt_ptr->table;
+}
---
+++ test/brwt/wavelet_tree_test.cpp
@@ -0,0 +1,373 @@
+#include "doctest.h"
+#include <brwt/wavelet_tree.h>
+
+#include <brwt/int_vector.h> // int_vector
+#include <array>             // array
+#include <cstddef>           // size_t, ptrdiff_t
+#include <ostream>           // ostream
+#include <string>            // string
+#include <type_traits>       // is_nothrow_default_constructible, ...
+#include <vector>            // vector
+
+using brwt::wavelet_tree;
+using brwt::int_vector;
+using std::size_t;
+using std::ptrdiff_t;
+using symbol_id = wavelet_tree::symbol_id;
+
+static constexpr auto map_upper(const char c) {
+  return static_cast<symbol_id>(c - 'A');
+}
+
+static constexpr auto to_unsigned(const ptrdiff_t value) {
+  return static_cast<size_t>(value);
+}
+
+static constexpr auto to_signed(const size_t value) {
+  return static_cast<ptrdiff_t>(value);
+}
+
+// Creates an int_vector with 2 bits per element
+static int_vector create_vector_with_2_bpe() {
+  std::array<unsigned, 24> arr = {
+      {0, 2, 2, 1, 2, 3, 1, 3, 2, 1, 3, 0, 0, 1, 2, 0, 1, 0, 0, 0, 3, 3, 2, 1}};
+
+  int_vector vec(arr.size(), /*bpe=*/2);
+  for (size_t i = 0; i < arr.size(); ++i) {
+    vec[to_signed(i)] = arr[i];
+  }
+
+  return vec;
+}
+
+static int_vector create_vector_with_3_bpe() {
+  const std::string str = "EHDHACEEGBCBGCF";
+  int_vector vec(to_signed(str.size()), /*bpe=*/3);
+
+  for (size_t i = 0; i < str.size(); ++i) {
+    using value_t = int_vector::value_type;
+    const auto value = static_cast<value_t>(map_upper(str[i]));
+    vec[to_signed(i)] = value;
+  }
+
+  return vec;
+}
+
+static auto to_std_vector(const int_vector& vec) {
+  std::vector<symbol_id> res(to_unsigned(vec.length()));
+  for (size_t i = 0; i < res.size(); ++i) {
+    res[i] = vec[to_signed(i)];
+  }
+  return res;
+}
+
+static auto to_std_vector(const wavelet_tree& wt) {
+  std::vector<symbol_id> res(to_unsigned(wt.size()));
+  for (size_t i = 0; i < res.size(); ++i) {
+    res[i] = wt.access(to_signed(i));
+  }
+  return res;
+}
+
+static auto to_string(const wavelet_tree::node_proxy& node) {
+  std::string str(to_unsigned(node.size()), '\0');
+  for (size_t i = 0; i < str.size(); ++i) {
+    str[i] = node.access(to_signed(i)) ? '1' : '0';
+  }
+  return str;
+}
+
+namespace std {
+static std::ostream& operator<<(std::ostream& os,
+                                const std::vector<symbol_id>& vec) {
+  os << '\n';
+  os << '[';
+  for (size_t i = 0; i + 1 < vec.size(); ++i) {
+    os << vec[i] << ", ";
+  }
+  if (!vec.empty()) {
+    os << vec.back();
+  }
+  os << ']';
+  return os;
+}
+} // end namespace std
+
+TEST_SUITE("wavelet_tree");
+
+template <typename T>
+static void test_wavelet_tree_default_member_functions() {
+  static_assert(std::is_nothrow_default_constructible<T>::value, "");
+  static_assert(std::is_copy_constructible<T>::value, "");
+  static_assert(std::is_nothrow_move_constructible<T>::value, "");
+  static_assert(std::is_copy_assignable<T>::value, "");
+  static_assert(std::is_nothrow_move_assignable<T>::value, "");
+  static_assert(std::is_nothrow_destructible<T>::value, "");
+}
+
+template <typename T>
+static void test_node_proxy_default_member_functions() {
+  static_assert(!std::is_default_constructible<T>::value, "");
+  static_assert(std::is_trivially_copy_constructible<T>::value, "");
+  static_assert(std::is_trivially_move_constructible<T>::value, "");
+  static_assert(std::is_trivially_copy_assignable<T>::value, "");
+  static_assert(std::is_trivially_move_assignable<T>::value, "");
+  static_assert(std::is_trivially_destructible<T>::value, "");
+}
+
+TEST_CASE("Default constructor") {
+  const wavelet_tree wt{};
+  CHECK(wt.size() == 0);
+  CHECK(wt.get_bits_per_symbol() == 0);
+  CHECK(wt.max_symbol_id() == 0);
+
+  // static asserts for the wavelet tree go here.
+  test_wavelet_tree_default_member_functions<wavelet_tree>();
+  test_node_proxy_default_member_functions<wavelet_tree::node_proxy>();
+}
+
+TEST_CASE("Constructor from int_vector") {
+  {
+    const wavelet_tree wt(create_vector_with_2_bpe());
+    CHECK(wt.size() == 24);
+    CHECK(wt.get_bits_per_symbol() == 2);
+    CHECK(wt.max_symbol_id() == 3);
+  }
+  {
+    const wavelet_tree wt(create_vector_with_3_bpe());
+    CHECK(wt.size() == 15);
+    CHECK(wt.get_bits_per_symbol() == 3);
+    CHECK(wt.max_symbol_id() == 7);
+  }
+}
+
+TEST_CASE("Access with sigma=4") {
+  const auto vec = create_vector_with_2_bpe();
+  const auto wt = wavelet_tree(vec);
+  // Explicit tests
+  CHECK(wt.access(1) == 2);
+  CHECK(wt.access(7) == 3);
+  CHECK(wt.access(19) == 0);
+  // Full range check
+  CHECK(to_std_vector(wt) == to_std_vector(vec));
+}
+
+TEST_CASE("Access with sigma=8") {
+  auto vec = create_vector_with_3_bpe();
+  const auto wt = wavelet_tree(vec);
+  // Explicit tests
+  CHECK(wt.access(1) == map_upper('H'));
+  CHECK(wt.access(6) == map_upper('E'));
+  CHECK(wt.access(14) == map_upper('F'));
+  // Full range check
+  CHECK(to_std_vector(wt) == to_std_vector(vec));
+}
+
+TEST_CASE("Rank with sigma=4") {
+  const auto wt = wavelet_tree(create_vector_with_2_bpe());
+  // seq = 0221 2313 2130 0120 1000 3321
+  REQUIRE(wt.size() == 24);
+
+  CHECK(wt.rank(/*symbol=*/0, /*pos=*/0) == 1);
+  CHECK(wt.rank(/*symbol=*/0, /*pos=*/8) == 1);
+  CHECK(wt.rank(/*symbol=*/0, /*pos=*/11) == 2);
+  CHECK(wt.rank(/*symbol=*/0, /*pos=*/13) == 3);
+  CHECK(wt.rank(/*symbol=*/0, /*pos=*/23) == 7);
+
+  CHECK(wt.rank(/*symbol=*/1, /*pos=*/0) == 0);
+  CHECK(wt.rank(/*symbol=*/1, /*pos=*/5) == 1);
+  CHECK(wt.rank(/*symbol=*/1, /*pos=*/12) == 3);
+  CHECK(wt.rank(/*symbol=*/1, /*pos=*/13) == 4);
+  CHECK(wt.rank(/*symbol=*/1, /*pos=*/23) == 6);
+
+  CHECK(wt.rank(/*symbol=*/2, /*pos=*/0) == 0);
+  CHECK(wt.rank(/*symbol=*/2, /*pos=*/4) == 3);
+  CHECK(wt.rank(/*symbol=*/2, /*pos=*/12) == 4);
+  CHECK(wt.rank(/*symbol=*/2, /*pos=*/22) == 6);
+  CHECK(wt.rank(/*symbol=*/2, /*pos=*/23) == 6);
+
+  CHECK(wt.rank(/*symbol=*/3, /*pos=*/0) == 0);
+  CHECK(wt.rank(/*symbol=*/3, /*pos=*/4) == 0);
+  CHECK(wt.rank(/*symbol=*/3, /*pos=*/5) == 1);
+  CHECK(wt.rank(/*symbol=*/3, /*pos=*/17) == 3);
+  CHECK(wt.rank(/*symbol=*/3, /*pos=*/23) == 5);
+}
+
+TEST_CASE("Rank with sigma=8") {
+  const auto wt = wavelet_tree(create_vector_with_3_bpe());
+  // seq = EHDHA CEEGB CBGCF
+  REQUIRE(wt.size() == 15);
+
+  auto rank = [&](const char symbol, const wavelet_tree::size_type pos) {
+    return wt.rank(map_upper(symbol), pos);
+  };
+
+  CHECK(rank('A', 0) == 0);
+  CHECK(rank('C', 0) == 0);
+  CHECK(rank('E', 0) == 1);
+  CHECK(rank('G', 0) == 0);
+
+  CHECK(rank('B', 5) == 0);
+  CHECK(rank('B', 8) == 0);
+  CHECK(rank('B', 9) == 1);
+  CHECK(rank('B', 10) == 1);
+  CHECK(rank('B', 11) == 2);
+
+  CHECK(rank('C', 4) == 0);
+  CHECK(rank('C', 5) == 1);
+  CHECK(rank('E', 10) == 3);
+  CHECK(rank('G', 10) == 1);
+
+  CHECK(rank('A', 14) == 1);
+  CHECK(rank('B', 14) == 2);
+  CHECK(rank('C', 14) == 3);
+  CHECK(rank('D', 14) == 1);
+  CHECK(rank('E', 14) == 3);
+  CHECK(rank('F', 14) == 1);
+  CHECK(rank('G', 14) == 2);
+  CHECK(rank('H', 14) == 2);
+}
+
+TEST_CASE("Select with sigma=4") {
+  const auto wt = wavelet_tree(create_vector_with_2_bpe());
+  // seq = 0221 2313 2130 0120 1000 3321
+  REQUIRE(wt.size() == 24);
+
+  // Random queries
+  CHECK(wt.select(/*symbol=*/0, /*nth=*/4) == 15);
+  CHECK(wt.select(/*symbol=*/2, /*nth=*/3) == 4);
+  CHECK(wt.select(/*symbol=*/1, /*nth=*/2) == 6);
+  CHECK(wt.select(/*symbol=*/3, /*nth=*/3) == 10);
+  CHECK(wt.select(/*symbol=*/2, /*nth=*/4) == 8);
+  CHECK(wt.select(/*symbol=*/1, /*nth=*/5) == 16);
+  CHECK(wt.select(/*symbol=*/0, /*nth=*/4) == 15);
+  CHECK(wt.select(/*symbol=*/3, /*nth=*/1) == 5);
+
+  // Last occurrences
+  CHECK(wt.select(/*symbol=*/0, /*nth=*/7) == 19);
+  CHECK(wt.select(/*symbol=*/1, /*nth=*/6) == 23);
+  CHECK(wt.select(/*symbol=*/2, /*nth=*/6) == 22);
+  CHECK(wt.select(/*symbol=*/3, /*nth=*/5) == 21);
+
+  // Barely out of range.
+  CHECK(wt.select(/*symbol=*/0, /*nth=*/8) == -1);
+  CHECK(wt.select(/*symbol=*/1, /*nth=*/7) == -1);
+  CHECK(wt.select(/*symbol=*/2, /*nth=*/7) == -1);
+  CHECK(wt.select(/*symbol=*/3, /*nth=*/6) == -1);
+
+  // Totally out of range.
+  CHECK(wt.select(/*symbol=*/0, /*nth=*/190) == -1);
+  CHECK(wt.select(/*symbol=*/1, /*nth=*/1312) == -1);
+  CHECK(wt.select(/*symbol=*/2, /*nth=*/122) == -1);
+  CHECK(wt.select(/*symbol=*/3, /*nth=*/423) == -1);
+}
+
+TEST_CASE("Select with sigma=8") {
+  const auto wt = wavelet_tree(create_vector_with_3_bpe());
+  // seq = EHDHA CEEGB CBGCF
+  REQUIRE(wt.size() == 15);
+
+  auto select = [&](const char symbol, const wavelet_tree::size_type pos) {
+    return wt.select(map_upper(symbol), pos);
+  };
+
+  CHECK(select('A', 1) == 4);
+  CHECK(select('A', 2) == -1);
+
+  CHECK(select('B', 1) == 9);
+  CHECK(select('B', 2) == 11);
+  CHECK(select('B', 3) == -1);
+
+  CHECK(select('C', 1) == 5);
+  CHECK(select('C', 2) == 10);
+  CHECK(select('C', 3) == 13);
+  CHECK(select('C', 4) == -1);
+
+  CHECK(select('D', 1) == 2);
+  CHECK(select('D', 2) == -1);
+
+  CHECK(select('E', 1) == 0);
+  CHECK(select('E', 2) == 6);
+  CHECK(select('E', 3) == 7);
+  CHECK(select('E', 4) == -1);
+
+  CHECK(select('F', 1) == 14);
+  CHECK(select('F', 2) == -1);
+
+  CHECK(select('G', 1) == 8);
+  CHECK(select('G', 2) == 12);
+  CHECK(select('G', 3) == -1);
+
+  CHECK(select('H', 1) == 1);
+  CHECK(select('H', 2) == 3);
+  CHECK(select('H', 3) == -1);
+
+  // Totally out of range.
+  CHECK(select('A', 4) == -1);
+  CHECK(select('B', 13) == -1);
+  CHECK(select('E', 74) == -1);
+  CHECK(select('H', 9923) == -1);
+}
+
+TEST_CASE("size, get_bits_per_symbol, max_symbol_id") {
+  {
+    const auto wt = wavelet_tree(int_vector(34, /*bpe=*/8));
+    CHECK(wt.size() == 34);
+    CHECK(wt.get_bits_per_symbol() == 8);
+    CHECK(wt.max_symbol_id() == 255);
+  }
+  {
+    const auto wt = wavelet_tree(int_vector(411, /*bpe=*/10));
+    CHECK(wt.size() == 411);
+    CHECK(wt.get_bits_per_symbol() == 10);
+    CHECK(wt.max_symbol_id() == 1023);
+  }
+}
+
+TEST_CASE("Navigation in WT with sigma=4") {
+  const auto wt = wavelet_tree(create_vector_with_2_bpe());
+  // seq = 022123132130012010003321
+
+  const auto root = wt.make_root();
+  const auto node_0 = root.make_lhs();
+  const auto node_1 = root.make_rhs();
+
+  CHECK(!root.is_leaf());
+  CHECK(node_0.is_leaf());
+  CHECK(node_1.is_leaf());
+
+  CHECK(to_string(root) == "011011011010001000001110");
+  CHECK(to_string(node_0) == "0111001010001");
+  CHECK(to_string(node_1) == "00011010110");
+}
+
+TEST_CASE("Navigation in WT with sigma=8") {
+  const auto wt = wavelet_tree(create_vector_with_3_bpe());
+  // seq = EHDHACEEGBCBGCF
+
+  const auto root = wt.make_root();
+  const auto node_0 = root.make_lhs();
+  const auto node_1 = root.make_rhs();
+  const auto node_00 = node_0.make_lhs();
+  const auto node_01 = node_0.make_rhs();
+  const auto node_10 = node_1.make_lhs();
+  const auto node_11 = node_1.make_rhs();
+
+  CHECK(!root.is_leaf());
+  CHECK(!node_0.is_leaf());
+  CHECK(!node_1.is_leaf());
+  CHECK(node_00.is_leaf());
+  CHECK(node_01.is_leaf());
+  CHECK(node_10.is_leaf());
+  CHECK(node_11.is_leaf());
+
+  CHECK(to_string(root) == "110100111000101");
+  CHECK(to_string(node_0) == "1010101");
+  CHECK(to_string(node_1) == "01100110");
+  CHECK(to_string(node_00) == "011");
+  CHECK(to_string(node_01) == "1000");
+  CHECK(to_string(node_10) == "0001");
+  CHECK(to_string(node_11) == "1100");
+}
+
+TEST_SUITE_END();
