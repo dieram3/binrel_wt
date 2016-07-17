082ed2c75778f1de730fde564f2bd0b945d32d7a
Diego <dieram3@gmail.com>
2016-07-17 16:58:49 -0400

    Corrected matrix assertion: no pair will have label equal to 5

--- test/brwt/binary_relation_test.cpp
+++ test/brwt/binary_relation_test.cpp
@@ -1,7 +1,7 @@
 #include "doctest.h"
 #include <brwt/binary_relation.h>
 
-#include <algorithm>        // shuffle
+#include <algorithm>        // shuffle, for_each
 #include <cassert>          // assert
 #include <initializer_list> // initializer_list
 #include <random>           // default_random_engine
@@ -13,11 +13,13 @@ using pair_type = binary_relation::pair_type;
 
 static binary_relation make_test_binary_relation() {
   std::vector<pair_type> pairs;
+  pairs.reserve(40);
+
   auto add_pairs = [&](const object_id object,
                        const std::initializer_list<label_id> labels) {
-    for (const auto label : labels) {
+    std::for_each(begin(labels), end(labels), [&](const auto label) {
       pairs.push_back({object, label});
-    }
+    });
   };
 
   // In the following matrix, objects are rows and labels are columns.
@@ -36,8 +38,8 @@ static binary_relation make_test_binary_relation() {
   //  6 |x|_|x|_|_|_|_|_|_|_|
   //  7 |_|x|_|x|_|_|x|_|x|_|
   //  8 |_|x|_|x|x|_|_|x|x|x|
-  //  9 |x|_|x|_|_|x|_|x|_|_|
-  // 10 |_|_|_|x|_|x|_|x|_|x|
+  //  9 |x|_|x|_|_|_|x|x|_|_|
+  // 10 |_|_|_|x|x|_|_|x|_|x|
   // 11 |_|x|x|_|x|_|_|_|x|_|
   //
   // Intentionally, no pair has label_id = 5
@@ -50,10 +52,13 @@ static binary_relation make_test_binary_relation() {
   add_pairs(6, {0, 2});
   add_pairs(7, {1, 3, 6, 8});
   add_pairs(8, {1, 3, 4, 7, 8, 9});
-  add_pairs(9, {0, 2, 5, 7});
-  add_pairs(10, {3, 5, 7, 9});
+  add_pairs(9, {0, 2, 6, 7});
+  add_pairs(10, {3, 4, 7, 9});
   add_pairs(11, {1, 2, 4, 8});
+
   assert(pairs.size() == 40);
+  assert(std::none_of(begin(pairs), end(pairs),
+                      [](const pair_type& p) { return p.label == 5; }));
 
   std::shuffle(begin(pairs), end(pairs), std::default_random_engine{});
   return binary_relation(pairs);
@@ -86,6 +91,13 @@ TEST_CASE("Rank with max object, max label") {
   CHECK(binrel.rank(7, 6) == 16);
   CHECK(binrel.rank(8, 2) == 8);
   CHECK(binrel.rank(8, 3) == 11);
+
+  CHECK(binrel.rank(7, 4) == 14);
+  CHECK(binrel.rank(7, 5) == 14);
+  CHECK(binrel.rank(7, 6) == 16);
+  CHECK(binrel.rank(11, 4) == 24);
+  CHECK(binrel.rank(11, 5) == 24);
+  CHECK(binrel.rank(11, 6) == 27);
 
   CHECK(binrel.rank(10, 8) == 33);
   CHECK(binrel.rank(10, 9) == 36);
