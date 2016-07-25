08bff681c7872b4b8ad29d26415996d65a480ad9
Diego <dieram3@gmail.com>
2016-07-25 04:40:36 -0400

    Added implementation

--- lib/int_vector.cpp
+++ lib/int_vector.cpp
@@ -3,7 +3,7 @@
 #include <brwt/bit_hacks.h> // lsb_mask, used_bits
 #include <algorithm>        // for_each, max, copy
 #include <cassert>          // assert
-#include <iterator>         // begin, end
+#include <iterator>         // begin, end, distance
 #include <limits>           // numeric_limits
 #include <stdexcept>        // domain_error
 
@@ -27,6 +27,19 @@ static int needed_bits(const InputRange& range) {
 // int_vector implementation
 // ==========================================
 
+auto int_vector::set_value(const size_type pos, const value_type value) noexcept
+    -> void {
+  assert(pos >= 0 && pos < num_elems && "Out of range");
+  assert(value <= lsb_mask<value_type>(static_cast<int>(bits_per_element)));
+
+  bit_seq.set_chunk(pos * bits_per_element, bits_per_element, value);
+}
+
+auto int_vector::non_const(const_iterator pos) noexcept -> iterator {
+  assert(pos >= begin() && pos <= end());
+  return begin() + (pos - cbegin());
+}
+
 int_vector::int_vector(const size_type count, const int bpe)
     : bit_seq{}, num_elems{count}, bits_per_element{bpe} {
   assert(count >= 0);
@@ -46,12 +59,20 @@ int_vector::int_vector(std::initializer_list<value_type> ilist)
   std::copy(ilist.begin(), ilist.end(), begin());
 }
 
-void int_vector::set_value(const size_type pos,
-                           const value_type value) noexcept {
-  assert(pos >= 0 && pos < num_elems && "Out of range");
-  assert(value <= lsb_mask<value_type>(static_cast<int>(bits_per_element)));
+auto int_vector::erase(const_iterator pos) noexcept -> iterator {
+  assert(pos >= begin() && pos < end());
+  return erase(pos, pos + 1);
+}
 
-  bit_seq.set_chunk(pos * bits_per_element, bits_per_element, value);
+auto int_vector::erase(const_iterator first, const_iterator last) noexcept
+    -> iterator {
+  assert(first >= begin() && first <= last && last <= end());
+
+  // Note that num_elems must be updated at the end because of cend() usage.
+  const auto new_end = std::copy(last, cend(), non_const(first));
+  num_elems = std::distance(begin(), new_end);
+
+  return non_const(first);
 }
 
 } // end namespace brwt
