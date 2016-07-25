b941e7b518d9d7e5f0e66167d3fa7c70c0d05292
Jorge Aguirre <jorge.aguirre.sza@gmail.com>
2016-07-25 19:30:53 -0400

    Add more tests
    Also uses a initializer_list to construct int_vectors

--- test/brwt/wavelet_tree/algorithms_test.cpp
+++ test/brwt/wavelet_tree/algorithms_test.cpp
@@ -13,31 +13,20 @@ static constexpr symbol_id operator"" _sym(const unsigned long long value) {
   return static_cast<symbol_id>(value);
 }
 
-static constexpr auto to_signed(const size_t value) {
-  return static_cast<ptrdiff_t>(value);
-}
-
 TEST_SUITE("wavelet_tree/algorithms");
 
 TEST_CASE("[select_first] small alphabet") {
-  std::array<unsigned, 24> arr = {
-      {0, 2, 2, 1, 2, 3, 1, 3, 2, 1, 3, 0, 0, 1, 2, 0, 1, 0, 0, 0, 3, 3, 2, 1}};
-
-  int_vector vec(arr.size(), /*bpe=*/2);
-  for (size_t i = 0; i < arr.size(); ++i) {
-    vec[to_signed(i)] = arr[i];
-  }
+  int_vector vec = {0, 2, 2, 1, 2, 3, 1, 3, 2, 1, 3, 0,
+                    0, 1, 2, 0, 1, 0, 0, 0, 3, 3, 2, 1};
 
   const auto wt = wavelet_tree(vec);
-
-  REQUIRE(wt.size() == 24);
 
   const auto select_first = [&](index_type start, symbol_id min,
                                 symbol_id max) {
     return brwt::select_first(wt, start, brwt::between<symbol_id>{min, max});
   };
 
-  // seq = 02212 3132 1300 1201 0003 321
+  // seq = 0221 2313 2130 0120 1000 3321
 
   CHECK(select_first(/*start=*/0, 0_sym, 1_sym) == 0);
   CHECK(select_first(/*start=*/0, 1_sym, 1_sym) == 3);
@@ -65,24 +54,17 @@ TEST_CASE("[select_first] small alphabet") {
 }
 
 TEST_CASE("[select_first] medium alphabet") {
-  std::array<unsigned, 24> arr = {
-      {0, 2, 2, 6, 5, 3, 6, 3, 2, 1, 3, 2, 0, 4, 5, 3, 7, 0, 0, 0, 3, 3, 2, 1}};
-
-  int_vector vec(arr.size(), /*bpe=*/3);
-  for (size_t i = 0; i < arr.size(); ++i) {
-    vec[to_signed(i)] = arr[i];
-  }
+  int_vector vec = {0, 2, 2, 6, 5, 3, 6, 3, 2, 1, 3, 2,
+                    0, 4, 5, 3, 7, 0, 0, 0, 3, 3, 2, 1};
 
   const auto wt = wavelet_tree(vec);
-
-  REQUIRE(wt.size() == 24);
 
   const auto select_first = [&](index_type start, symbol_id min,
                                 symbol_id max) {
     return brwt::select_first(wt, start, brwt::between<symbol_id>{min, max});
   };
 
-  // seq= 02265 3632 1320 4537 0003 321
+  // seq= 0226 5363 2132 0453 7000 3321
 
   CHECK(select_first(/*start=*/0, 0_sym, 1_sym) == 0);
   CHECK(select_first(/*start=*/0, 1_sym, 1_sym) == 9);
@@ -115,4 +97,65 @@ TEST_CASE("[select_first] medium alphabet") {
     CHECK(select_first(/*start=*/12, 6_sym, 6_sym) == index_npos);
     CHECK(select_first(/*start=*/13, 6_sym, 6_sym) == index_npos);
   }
+}
+
+TEST_CASE("[select_first] few nodes") {
+  int_vector vec = {0, 2, 2, 1};
+
+  const auto wt = wavelet_tree(vec);
+
+  const auto select_first = [&](index_type start, symbol_id min,
+                                symbol_id max) {
+    return brwt::select_first(wt, start, brwt::between<symbol_id>{min, max});
+  };
+
+  // seq = 0221
+
+  CHECK(select_first(/*start=*/0, 0_sym, 1_sym) == 0);
+  CHECK(select_first(/*start=*/1, 0_sym, 1_sym) == 3);
+  CHECK(select_first(/*start=*/1, 0_sym, 2_sym) == 1);
+  CHECK(select_first(/*start=*/1, 0_sym, 0_sym) == index_npos);
+
+  CHECK(select_first(/*start=*/0, 1_sym, 1_sym) == 3);
+  CHECK(select_first(/*start=*/0, 2_sym, 2_sym) == 1);
+  CHECK(select_first(/*start=*/0, 3_sym, 3_sym) == index_npos);
+
+  CHECK(select_first(/*start=*/3, 2_sym, 2_sym) == index_npos);
+  CHECK(select_first(/*start=*/3, 3_sym, 3_sym) == index_npos);
+}
+
+TEST_CASE("[select_first] one node") {
+  int_vector vec = {0, 0, 0, 1, 0, 0, 0, 0, 0};
+
+  const auto wt = wavelet_tree(vec);
+
+  const auto select_first = [&](index_type start, symbol_id min,
+                                symbol_id max) {
+    return brwt::select_first(wt, start, brwt::between<symbol_id>{min, max});
+  };
+
+  // seq = 0001 0000 0
+
+  CHECK(select_first(/*start=*/0, 0_sym, 0_sym) == 0);
+  CHECK(select_first(/*start=*/1, 0_sym, 0_sym) == 1);
+  CHECK(select_first(/*start=*/0, 0_sym, 1_sym) == 0);
+  CHECK(select_first(/*start=*/0, 1_sym, 1_sym) == 3);
+  CHECK(select_first(/*start=*/5, 1_sym, 1_sym) == index_npos);
+}
+
+TEST_CASE("[select_first] one node, one symbol") {
+  int_vector vec = {{0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+  const auto wt = wavelet_tree(vec);
+
+  const auto select_first = [&](index_type start, symbol_id min,
+                                symbol_id max) {
+    return brwt::select_first(wt, start, brwt::between<symbol_id>{min, max});
+  };
+
+  CHECK(select_first(/*start=*/0, 0_sym, 0_sym) == 0);
+  CHECK(select_first(/*start=*/0, 0_sym, 1_sym) == 0);
+  CHECK(select_first(/*start=*/0, 1_sym, 1_sym) == index_npos);
+  CHECK(select_first(/*start=*/4, 1_sym, 1_sym) == index_npos);
+  CHECK(select_first(/*start=*/8, 1_sym, 1_sym) == index_npos);
 }
