a21e917b9da963b678f65d96588b65d88c53dd24
Diego <dieram3@gmail.com>
2016-08-03 02:41:50 -0400

    Added CMake option to enable link time optimization.
    It will turn on the INTERPROCEDURAL_OPTIMIZATION property of all BRWT targets.
    If it is known that CMake ignores the property in the current environment even
    though the environment supports LTO, BRWT will add the necessary flags to
    activate it (for now, just the AppleClang case is handled).

--- cmake/modules/AddBRWT.cmake
+++ cmake/modules/AddBRWT.cmake
@@ -20,10 +20,32 @@ function(brwt_make_cpp14_strict TARGET_ID)
   set_property(TARGET ${TARGET_ID} PROPERTY CXX_STANDARD_REQUIRED ON)
 endfunction()
 
-# Default configuration for BRWT targets.
+# Adds flags to the LINK_FLAGS property of the given target.
+function(brwt_target_link_flags TARGET_ID)
+  foreach(FLAG ${ARGN})
+    set_property(TARGET ${TARGET_ID} APPEND_STRING
+                 PROPERTY LINK_FLAGS " ${FLAG}")
+  endforeach()
+endfunction()
+
+function(brwt_target_enable_lto TARGET_ID)
+  set_property(TARGET ${TARGET_ID} PROPERTY INTERPROCEDURAL_OPTIMIZATION ON)
+
+  # The current CMake version does nothing when AppleClang is used, so...
+  if (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    set(LTO_FLAG "-flto") # todo: Try -flto=thin when available.
+    brwt_target_link_flags(${TARGET_ID} ${LTO_FLAG})
+    target_compile_options(${TARGET_ID} PRIVATE ${LTO_FLAG})
+  endif()
+endfunction()
+
 function(brwt_configure TARGET_ID)
   brwt_enable_all_warnings(${TARGET_ID})
   brwt_make_cpp14_strict(${TARGET_ID})
+
+  if (BRWT_ENABLE_LTO)
+    brwt_target_enable_lto(${TARGET_ID})
+  endif()
 endfunction()
 
 function(add_brwt_library TARGET_ID)
