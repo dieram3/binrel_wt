9eacd9eb72316a79b085f759d24f2900b9695495
Diego Ramirez <dieram3@gmail.com>
2024-02-16 16:49:27 -0800

    [brwt] index_range_test: add dummy test to pacify include-cleaner

--- test/brwt/index_range_test.cpp
+++ test/brwt/index_range_test.cpp
@@ -1,4 +1,19 @@
 #include "brwt/index_range.h"
 #include <doctest/doctest.h>
 
-// TODO(Diego): Test this module.
+namespace brwt::test {
+namespace {
+
+// TEST_SUITE("index_range");
+
+TEST_CASE("index_range::index_range()") {
+  const index_range range;
+
+  CHECK(range.begin() == 0);
+  CHECK(range.end() == 0);
+}
+
+// TODO(Diego): Test the other stuff.
+
+} // namespace
+} // namespace brwt::test
